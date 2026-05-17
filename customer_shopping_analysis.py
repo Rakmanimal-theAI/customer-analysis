d8a85ea9e8f0588f759db15c13eb54879eb1b54c
Nicolas Mir <nicolasmir@Nicolass-MacBook-Air.local>
2026-05-18 10:45:51 +1200

    Data cleaning and querying

---
+++ customer_shopping_analysis.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+df = pd.read_csv('customer_shopping_behavior.csv')
+
+print(df.head())
+
+print(df.describe(include="all"))
+
+print(df.isnull().sum())
+
+# Replace Nulls with median within each category
+df['Review Rating'] = df.groupby('Category')['Review Rating'].transform(lambda x: x.fillna(x.median()))
+
+print(df.isnull().sum())
+
+df.columns = df.columns.str.lower()
+df.columns = df.columns.str.replace(' ', '_')
+df = df.rename(columns={'purchase_amount_(usd)': 'purchase_amount'})
+
+print(df.columns)
+
+# Create Age Group
+labels = ['Young Adult', 'Adult', 'Middle-aged', 'Senior']
+df['age_group'] = pd.qcut(df['age'], q=4, labels = labels)
+
+print(df[['age', 'age_group']].head(10))
+
+# Create Purchase frequency days
+frequency_mapping = {
+    'Fortnightly': 14,
+    'Weekly': 7,
+    'Monthly': 30,
+    'Quarterly': 90,
+    'Bi-Weekly': 14,
+    'Annually': 365,
+    'Every 3 Months': 90
+}
+
+df['purchase_frequency_days'] = df['frequency_of_purchases'].map(frequency_mapping)
+
+print(df[['purchase_frequency_days', 'frequency_of_purchases']].head(10))
+
+print(df[['discount_applied', 'promo_code_used']].head(10))
+
+print((df['discount_applied'] == df['promo_code_used']).all())
+
+df = df.drop('promo_code_used', axis=1)
+
+print(df.columns)
+
+from sqlalchemy import create_engine
+
+# MySQL connection
+username = "root"
+password = "password123"
+host = "localhost"
+port = "3306"
+database = "customer_behavior"
+
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}")
+
+# Write DataFrame to MySQL
+table_name = "customer"   # choose any table name
+df.to_sql(table_name, engine, if_exists="replace", index=False)
+
+# Read back sample
+print(pd.read_sql("SELECT * FROM customer LIMIT 5;", engine))
